4ac1770221ebb8a678f05e8dab51472ffb8fd17e
niloymondal <niloymond25@gmail.com>
2024-08-08 17:57:34 +0600

    progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="./login" element={<LoginSignUp />} />
           </Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
