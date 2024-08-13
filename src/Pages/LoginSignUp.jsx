311e293e9406c1a2cdcff51bb37f8f75b4732f9b
niloymondal <niloymond25@gmail.com>
2024-08-13 18:05:45 +0600

    progress3

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -2,7 +2,21 @@
  
  const LoginSignUp = () => {
    return (
-     <div>
+     <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginssignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By contunuing, I agree to the terms of use and policy</p>
+        </div>
+      </div>
        
      </div>
    );
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
-import dropdown_icon from "../Components/Assets/dropdown_icon.png"
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Items from "../Components/Items/Items";
 
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
@@ -10,18 +11,34 @@ const ShopCategory = (props) => {
       <img src={props.banner} alt="" />
 
       <div className="shopcategory-indexSort">
-      <p>
-        <span>Showing 1-12</span>out of 36 products
-      </p>
-      <div className="shopcategory-sort">
-        Sort by <img src={dropdown_icon} alt="" />
+        <p>
+          <span>Showing 1-12</span>out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=>){
-          
-        }}
-      </div>
+  {all_product.map((item, i) => {
+    if (props.category === item.category) {
+      return (
+        <Items
+          key={i}
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          new_price={item.new_price}
+          old_price={item.old_price}
+        />
+      );
+    } else {
+      return null;
+    }
+  })}
+</div>
+<div className="shopcategory-loadmore">
+  Explore More
+</div>
     </div>
   );
 };
